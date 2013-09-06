697b5d0301e42a03172e3dca773cb1cd18cb7de8
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2013-09-06 08:35:40 -0700

    edits to unit2

--- lectures/unit2-bash.sh
+++ lectures/unit2-bash.sh
@@ -209,7 +209,7 @@ function niceR(){
 
 for file in $(ls *txt)
 do
-   mv $file ${file/.txt/.q} # this syntax replaces .txt with .q in $file
+   mv $file ${file/.txt/.R} # this syntax replaces .txt with .q in $file
 done
 
 
@@ -246,7 +246,7 @@ done
 n=100 # if I want to be able to vary n from outside the R program
 for(( it=1; it<=100; it++));
 do
-    echo "n=$n; it=$it; source('base.q')" > tmp-$n-$it.q
-    R CMD BATCH --no-save tmp-$n-$it.q sim-n$n-it$it.Rout&
+    echo "n=$n; it=$it; source('base.R')" > tmp-$n-$it.R
+    R CMD BATCH --no-save tmp-$n-$it.R sim-n$n-it$it.Rout&
 done
-# note that base.q should NOT set either 'n' or 'it', but should make use of them, including when creating unique names for output files for each iteration
+# note that base.R should NOT set either 'n' or 'it', but should make use of them, including when creating unique names for output files for each iteration
